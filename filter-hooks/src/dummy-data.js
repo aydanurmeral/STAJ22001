ba8da80943178d1a8ec53f5b1ea64982f8e8b75e
Aydanur MERAL <aydamrl511@icloud.com>
2024-09-10 16:26:04 +0300

    hooks

---
+++ filter-hooks/src/dummy-data.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    id: 'ayda',
+    fullName: 'aydanur meral',
+    projects: [
+      { id: 'ayda_p1', title: 'learn vue' },
+      { id: 'ayda_p2', title: 'do ts projects' },
+      { id: 'ayda_p3', title: 'solve error' }
+    ]
+  },
+  {
+    id: 'merve',
+    fullName: 'merve sardoğan',
+    projects: [
+      { id: 'manu_p1', title: 'learn node.js' },
+      { id: 'manu_p2', title: 'Work with API' },
+    ]
+  },
+  {
+    id: 'burak',
+    fullName: 'burak mumcu',
+    projects: []
+  }
+];
